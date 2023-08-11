3caff556c517546935fb90205ed7c357f11fa359
Madhu-1607 <94669981+Madhu-1607@users.noreply.github.com>
2023-08-11 11:30:56 +0530

    Update register.component.ts

--- FrontEnd/src/app/register/register.component.ts
+++ FrontEnd/src/app/register/register.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../auth.service';
 import { Restaurant } from '../Restaurant';
 import { Router } from '@angular/router';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -30,14 +30,50 @@ export class RegisterComponent {
       .subscribe(
         (response:any) => {
           console.log(response);
-          alert('Registration Successful!');
+          // alert('Registration Successful!');
+          const style = document.createElement('style');
+style.innerHTML = `
+  .background-pink{
+    background-color: #A47E3B !important; /* Baby pink color */
+  }
+`;
+
+document.head.appendChild(style);
+        Swal.fire({
+          title: '<span style="color:whitesmoke;">Thank You!</span>',
+  html: '<span style="color:whitesmoke;">Registration Successfull</span>',
+  icon: 'success',
+  confirmButtonColor: '#61481C',
+  customClass:
+  {
+    popup:'background-pink'
+  },
+        }),
           this.router.navigate(['/login']);
 
           // Handle success message or redirect to login page
         },
         error => {
           console.error(error);
-          alert('Registration Failed!');
+          // alert('Registration Failed!');
+          const style = document.createElement('style');
+style.innerHTML = `
+  .background-pink{
+    background-color: #A47E3B !important; /* Baby pink color */
+  }
+`;
+
+document.head.appendChild(style);
+        Swal.fire({
+          title: '<span style="color:whitesmoke;">Oops!</span>',
+  html: '<span style="color:whitesmoke;">Registration Failed</span>',
+  icon: 'error',
+  confirmButtonColor: '#61481C',
+  customClass:
+  {
+    popup:'background-pink'
+  },
+        })
           // Handle error message
         }
       );
